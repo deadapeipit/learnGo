c5c8e72026d03b25369d5e3388401848b484a464
deadapeipit <108110354+deadapeipit@users.noreply.github.com>
2022-06-27 10:31:40 +0700

    learn golang
    loop

---
+++ project-pertama/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	for j := 0; j < 11; j++ {
+		if j%2 == 1 {
+			fmt.Println("ganjil")
+
+		} else {
+			fmt.Println("genap")
+
+		}
+	}
+	//fmt.Println("Hello world")
+}
